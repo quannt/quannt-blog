2767b6bdc8fc67a30fdd84d6f4779514f3eb5f53
quannt <simpleplan.vn@gmail.com>
2020-07-11 23:53:43 +0900

    Update site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     // Can be used for e.g. JSONLD
     siteHeadline: `Quannt's Technical Blog`,
     // Will be used to generate absolute URLs for og:image etc.
-    siteUrl: `https://quannt.github.io`,
+    siteUrl: `https://quannt.xyz`,
     // Used for SEO
     siteDescription: `Quannt's Technical Blog`,
     // Will be set on the <html /> tag
