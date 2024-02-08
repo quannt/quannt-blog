6b1c229d120eb310399afc828d22f90db1f05e9a
quannt <simpleplan.vn@gmail.com>
2024-02-08 11:39:43 +0900

    Add analytics

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+ 
+import BaseFooter from '@lekoarts/gatsby-theme-minimal-blog/src/components/footer'
+
+const Footer = () => {
+  return <div>
+      <BaseFooter />
+      <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "c4d6db8b428d4ad7a8486d68e2fb551a"}'></script>
+  </div>
+  
+}
+
+export default Footer
