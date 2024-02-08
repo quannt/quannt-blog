2a67b4b83bbf3cf7a0f8d6377d38a2e76c00ef28
quannt <simpleplan.vn@gmail.com>
2024-02-08 12:05:13 +0900

    Change posts limit

---
+++ src/@lekoarts/gatsby-theme-minimal-blog-core/templates/homepage-query.tsx
@@ -0,0 +1,25 @@
+import { graphql } from "gatsby"
+import HomepageComponent, { Head } from "@lekoarts/gatsby-theme-minimal-blog-core/src/components/homepage"
+
+export default HomepageComponent
+
+export { Head }
+
+export const query = graphql`
+  query ($formatString: String!) {
+    allPost(sort: { date: DESC }, limit: 100) {
+      nodes {
+        slug
+        title
+        date(formatString: $formatString)
+        excerpt
+        timeToRead
+        description
+        tags {
+          name
+          slug
+        }
+      }
+    }
+  }
+`
